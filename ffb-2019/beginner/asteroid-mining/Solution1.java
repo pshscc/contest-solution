49d5f946e4cea2fdefa09adcb58bb66d25c01fee
Jeremy Zhang <41759551+jeremyzhang1@users.noreply.github.com>
2019-10-23 00:12:30 -0500

    Create Solution1.java

---
+++ ffb-2019/beginner/asteroid-mining/Solution1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution1 {
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    double n = in.nextDouble();
+    int m = 0;
+    int k = 1;
+    while(n > 20){
+      n /= 3;
+      m += k;
+      k *= 3;
+    }
+    System.out.println(m);
+  }
+}
