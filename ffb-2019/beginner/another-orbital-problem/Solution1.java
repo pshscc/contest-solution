b0cb036de217844f853a6043c665a34b019c19d1
Jeremy Zhang <41759551+jeremyzhang1@users.noreply.github.com>
2019-10-22 23:08:08 -0500

    Create Solution1.java

---
+++ ffb-2019/beginner/another-orbital-problem/Solution1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextInt()/4);
+    }
+}
