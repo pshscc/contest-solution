9fa8c2c9996cae6ec479f2d5ba267c2d078ddd0a
John (JB) Ladera <domobaby10@gmail.com>
2019-10-22 21:38:01 -0500

    fixed class name

--- ffb-2019/novice/which-rock-sample/Solution1.java
+++ ffb-2019/novice/which-rock-sample/Solution1.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class Solution1 {
 
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
