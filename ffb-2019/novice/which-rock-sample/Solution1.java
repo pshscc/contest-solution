ae21cac68bfeed627380cbf24925ec771d1f8537
Shakeel <shakeelsamsudeen15@gmail.com>
2019-10-22 21:24:21 -0500

    solved which-rock-sample

---
+++ ffb-2019/novice/which-rock-sample/Solution1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line = "";
+        while((line = in.readLine()) != null) {
+            if(line.length() == 1) continue; // workaround for bad judge data
+            String[] input = line.split(" ");
+            int x = Integer.parseInt(input[0]);
+            int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+            for(int i = 1; i <= x; i++) {
+                int num = Integer.parseInt(input[i]);
+                min = Math.min(min, num);
+                max = Math.max(max, num);
+            }
+            if(min >= 3) {
+                System.out.println("Uh oh");
+            }            
+            else if(min == max)
+                System.out.println(min);
+            else
+                System.out.println(min + " " + max);
+        }
+    }
+}
