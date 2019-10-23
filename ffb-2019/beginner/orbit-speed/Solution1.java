8f3f6384248659769fa8455e9b706f643db96948
Jeremy Zhang <41759551+jeremyzhang1@users.noreply.github.com>
2019-10-22 23:36:39 -0500

    Create Solution1.java

---
+++ ffb-2019/beginner/orbit-speed/Solution1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int r = in.nextInt();
+        System.out.println((int)(r/(1-(r/(double)a))));
+    }
+}
