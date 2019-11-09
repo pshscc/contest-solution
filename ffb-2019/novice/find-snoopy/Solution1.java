5a47bffcc37f7ad441cfdabafad4717676f81004
Shakeel <shakeelsamsudeen15@gmail.com>
2019-11-08 21:16:43 -0600

    fixed solution

--- ffb-2019/novice/find-snoopy/Solution1.java
+++ ffb-2019/novice/find-snoopy/Solution1.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class Solution1 {
 
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
